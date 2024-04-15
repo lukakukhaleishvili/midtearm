c85b66a64d2784ba99ea651c9ec2705bc6b0eb66
lukakukhaleihvili <lukakukhaleishvili1818@@@gmail.com>
2024-04-15 18:44:11 +0400

    eddit files

---
+++ Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+
+namespace Reddit.Middlewares
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _requestDelegate;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+        public ExceptionHandlingMiddleware(RequestDelegate requestDelegate, ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            this._requestDelegate = requestDelegate;
+            this._logger = logger;
+        }
+
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+
+                await _requestDelegate(context);
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Exception thrown");
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                await context.Response.WriteAsync("Unexpected error occured on the server.");
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,8 +2,15 @@ using Microsoft.EntityFrameworkCore;
 using Reddit;
 using Reddit.Mapper;
 using System.Text.Json.Serialization;
+using Reddit.Middlewares;
+using Reddit.Filters;
+using Microsoft.Extensions.Options;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers(option => option.Filters.Add<ModelValidationActionFilter>());
 
 // Add services to the container.
 
@@ -33,6 +40,8 @@ builder.Services.AddCors(options =>
 builder.Services.AddSingleton<IMapper, Mapper>();
 
 
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -41,6 +50,18 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Add("Content-Type", "application/json");
+    await next();
+});
+
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+app.MapGet("/throws", (context) => throw new Exception("Unexpected error occured on the server."));
+
 
 app.UseHttpsRedirection();
 app.UseCors();
